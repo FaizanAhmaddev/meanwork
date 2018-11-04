72c38d28c619e190231d35fd2fa11d06863c6fda
Usman Zaheer <usmanzaheer1995@gmail.com>
2018-11-04 23:32:29 +0500

    'changes'

--- backend/app.js
+++ backend/app.js
@@ -44,5 +44,5 @@ app.use('*', (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server listening at port ${port}`);
+  console.log(`Server listening at port ${process.env.PORT}`);
 });
